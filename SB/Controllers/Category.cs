32318f7fcbac419fd8247ff03cf9d0e7d7198b08
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-06-05 15:11:54 +0400

    Add project files.

---
+++ SB/Controllers/Category.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SB.Controllers
+{
+    public class Category : Controller
+    {
+        public IActionResult Index(int idCategory)
+        {
+
+            return View(new SwapBookDbContext().Books.Where(c=>c.Id==idCategory).ToList());
+        }
+    }
+}
---
+++ SB/Galary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SB;
+
+public partial class Galary
+{
+    public int Id { get; set; }
+
+    public int? IdBook { get; set; }
+
+    public virtual Book? IdBookNavigation { get; set; }
+}
