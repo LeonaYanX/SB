69e21ab5c74707eb6b45a52732fb22a9e2867d7f
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-06-12 15:18:27 +0400

    with nullable Photo

--- SB/User.cs
+++ SB/User.cs
@@ -11,7 +11,7 @@ public partial class User
 
     public string Password { get; set; }
 
-    public byte[] Photo { get; set; }
+    public byte[] ?Photo { get; set; }
 
     public string Phone { get; set; }
 
